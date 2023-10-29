57155e4c2c02810641bba6557448cee15f7da197
toniiplaycode <toan13022017@gmail.com>
2023-10-29 15:32:29 +0700

    update

--- backend/server/controllers/noteController.js
+++ backend/server/controllers/noteController.js
@@ -25,3 +25,47 @@ export const createNote = async(req, res) => {
         throw new Error(error.message);
     }
 }
+
+export const editNote = async(req, res) => {
+    const { noteId, title, content } = req.body;
+
+    try {
+        if(!noteId || !content) {
+            res.status(400);
+            throw new Error("noteId or content empty !");
+            return;
+        }
+
+        const note = await Note.findByIdAndUpdate(noteId, {
+            title,
+            content
+        });
+        
+        if(note) res.status(200).json({message: "edit successed !"});
+
+    } catch (error) {
+        res.status(400);
+        throw new Error(error.message);
+    }
+}
+
+export const deleteNote = async(req, res) => {
+    const { noteId } = req.body;    
+    
+    try {
+        if(!noteId) {
+            res.status(400);
+            throw new Error("noteId is empty !");
+        }    
+
+        const note = await Note.findOneAndDelete({_id: noteId});
+        if(note) {
+            res.status(200).json({ message: "delete successed !" })
+        } else {
+            res.status(400).json({ message: "delete failed, cannot find noteId !" })
+        } ;
+    } catch (error) {
+        res.status(400);
+        throw new Error(error.message);
+    }
+}
--- backend/server/routes/apiNote.js
+++ backend/server/routes/apiNote.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { createNote } from "../controllers/noteController.js";
+import { createNote, deleteNote, editNote } from "../controllers/noteController.js";
 
 const router = express.Router();
 
 const initNoteRoutes = (app) => {
     router.post("/createNote", createNote);
+    router.post("/editNote", editNote);
+    router.post("/deleteNote", deleteNote);
 
     return app.use("/", router);
 }
